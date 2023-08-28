9063f531d6da64037484bc1e84a184d0de8aa790
jumma <aarifhingorja4@gmail.com>
2023-08-28 14:45:41 +0530

    added community

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -147,3 +147,33 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users: ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // find all threads created by the users
+
+    const userThreads = await Thread.find({author: userId});
+
+    // Collect all the child thread ids (replies) from the "children" field
+    const childThreadIds = userThreads.reduce((acc,userThread) => {
+      return acc.concat(userThread.children);
+    },[]);
+
+    // find all the threads that the user has replied to
+    const replies = await Thread.find({
+      _id: { $in : childThreadIds},
+      author: {$ne: userId}
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id"
+    });
+
+    return replies;
+
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`);
+  }
+}
